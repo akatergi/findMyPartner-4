efdd2f9b8bd61b6746b6ba1b3eb078ea89f4517c
akatergi <katergiadel@gmail.com>
2022-02-16 17:37:10 -0500

    l

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const cookieParser = require("cookie-parser")
 const { match } = require("./Tools")
 const isImageURL = require('image-url-validator').default;
 
-const dbURL = "mongodb+srv://user:adminPass12345@cluster0.am7j8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const dbURL = process.env.DB_URL
 mongoose.connect(dbURL).then(() => console.log('Database connected')).catch(e => console.log(e))
 const isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
